583377a8e9bbcbaddab42cad48ff9a7d1a69f863
Bader Mohammad <bader97alyami@hotmail.com>
2023-02-12 13:11:30 +0300

    n

--- invest-with-us/src/App.tsx
+++ invest-with-us/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import HomePage from './components/HomePage'
 import Show from './components/ShowCards';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import AddCompany from './components/AddCompany'
 import ShowCards from './components/ShowCards';
@@ -10,13 +10,17 @@ function App() {
  
   return (
     <div className="App">
-
+<Routes>
+        <Route path="/" element={<HomePage />}></Route>
+          <Route path="/ShowCards" element={<ShowCards/>} />
+      
+      </Routes>
 
       {/* <HomePage/> */}
 
-      <AddCompany/>
+      {/* <AddCompany/> */}
 
-      <ShowCards/>
+      {/* <ShowCards/> */}
 
       </div>
 
