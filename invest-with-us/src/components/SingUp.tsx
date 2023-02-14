e4f15ff0b4b434949aa467089845bf5de4449bfc
Amer <ameralkhanni@gmail.com>
2023-02-14 10:21:37 +0300

    add new imag

--- invest-with-us/src/components/SingUp.tsx
+++ invest-with-us/src/components/SingUp.tsx
@@ -10,15 +10,19 @@ function SingUp() {
 
       <Modal isOpen={isOpen} onClose={onClose} >
         <ModalOverlay />
-        <ModalContent w={'full'} h={'md'}  >
+        <ModalContent w={'full'} h={'lg'}  >
           <ModalHeader mx={'auto'} my={'25'}>Do you want to Sign Up as ?</ModalHeader>
           <ModalCloseButton />
           <ModalBody >
-           
+          <img
+        src='https://a.top4top.io/p_26012ivly1.png'
+        className='img-fluid rounded'
+        alt='example'
+      />
           </ModalBody>
 
           <ModalFooter mx={'auto'} >
-          <Link to='/SingUp'>  <Button bg={"green.400"} color='#fff' mr={3}>
+          <Link to='/SingUp'>  <Button bg={"green.400"} color='#fff' mr={"4rem"}>
           Company
             </Button>
             </Link>
